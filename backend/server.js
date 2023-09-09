e03549e5b5dc5d558ea93829c67728146c31d144
Sparsh Goel <sparshgoelt26@gmail.com>
2023-09-10 00:59:23 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
 
+// Database Connection
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Router
+const todo = require("./routes/todo");
+
+connectDB();
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
+
+app.use("/api/todo", todo);
 
 // setting up port
 const PORT = process.env.PORT || 8000;
