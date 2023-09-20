7df8622fe11d20b39e212990ca0393c200b49b92
Sparsh Goel <sparshgoelt26@gmail.com>
2023-09-20 16:35:27 +0530

    Added CRUD Functionalities

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
-import { ShowTodoList } from './components/showTodoList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ShowTodoList } from "./components/showTodoList";
+import { CreateTodo } from "./components/createTodo";
+import "./App.css";
 
 function App() {
-
-  return (
-    <div className="app-contents">
-      <ShowTodoList />
-    </div>
-  );
+    return (
+        <div className="app-contents">
+          <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<ShowTodoList />} />
+                <Route path="/create-todo" element={<CreateTodo />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
